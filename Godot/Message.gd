bca9f302525a6edf18bb5a43c566fab509464841
Arttu Lakkala <arttuje.lakkala@gmail.com>
2021-03-23 11:54:39 +0200

    added a closing button to message

--- Godot/Message.gd
+++ Godot/Message.gd
@@ -1,5 +1,14 @@
 extends Control
 
 
+var parent 
+
+func _ready():
+	parent = get_parent()
+
 func set_popup(content_text):
 		$Content.set_text(content_text)
+
+
+func _on_Button_pressed():
+	parent.closeMessage()
