b556ca223af2396fc8de7e560a2aa18c800fb7ea
Arttu Lakkala <arttuje.lakkala@gmail.com>
2021-03-25 21:27:00 +0200

    Added randomized good and bad messages

--- Godot/Message.gd
+++ Godot/Message.gd
@@ -6,10 +6,11 @@ var content
 var sender
 var player
 var tutorial = true
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	parent = get_parent()
-	
+	rng.randomize()
 
 func set_popup(messageType, content_text_number, sender = "Message from the Citizen's Council:"):
 	#visualize correct message form
@@ -17,6 +18,39 @@ func set_popup(messageType, content_text_number, sender = "Message from the Citi
 	$grandma.visible = false
 	$resident2.visible = false
 	$mageman.visible = false
+	
+	#randomize good and bad messages
+	#bad ones
+	if(content_text_number>15 && content_text_number<20):
+		#happens 66present of time
+		var rnumber = rng.randi_range(1,100)
+		if(rnumber<44):
+			pass
+		elif(rnumber<66):
+			content_text_number = 30
+			messageType = 2
+		elif(rnumber<88):
+			content_text_number = 31
+			messageType = 3
+		else:
+			content_text_number = 32
+			messageType = 4
+	#good ones
+	if(content_text_number>=25 && content_text_number<28):
+		#happens 66present of time
+		var rnumber = rng.randi_range(1,100)
+		if(rnumber<44):
+			pass
+		elif(rnumber<66):
+			pass
+		elif(rnumber<88):
+			content_text_number = 36
+			messageType = 3
+		else:
+			content_text_number = 37
+			messageType = 4
+	
+	
 	if messageType == 1:
 		player = $MessageAudio
 		content = get_node("browser/BrowserContent")
